00077917746b24f9c19eb2d37528f51039d07d74
Susanto <sushantomahato1@gmail.com>
2024-04-01 17:22:44 +0530

    assesment8_building Ecommerce

--- src/App.js
+++ src/App.js
@@ -18,15 +18,21 @@ const dummyMovies = [
 ];
 
 function App() {
-  const [moviesList, setMoviesList] = useState();
+  const [moviesList, setMoviesList] = useState([]);
   const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState(null);
+  const [reTry, setReTry] = useState(null);
+
   async function fetchMoviesHandler() {
     try {
       setIsloading(true);
+      setError(null);
       setMoviesList([]);
-      const response = await fetch("https://swapi.dev/api/films");
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Something Went Wrong... Retrying");
+      }
       const moviesData = await response.json();
-
       const transformedMovies = moviesData.results.map((movie) => {
         return {
           title: movie.title,
@@ -36,19 +42,44 @@ function App() {
         };
       });
       setMoviesList(transformedMovies);
-      setIsloading(false);
     } catch (err) {
-      console.log(err.message);
+      setError(err.message);
+      if (!reTry) {
+        let retrying = setTimeout(() => {
+          fetchMoviesHandler();
+        }, 5000);
+        setReTry(retrying);
+      }
+    } finally {
+      setIsloading(false);
     }
   }
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button
+          onClick={() => {
+            if (reTry) {
+              clearTimeout(reTry);
+              setReTry(null);
+              setError("Retrying Cancelled");
+            }
+          }}
+        >
+          Cancel
+        </button>
       </section>
       <section>
+        {!isLoading && moviesList?.length > 0 && (
+          <MoviesList movies={moviesList} />
+        )}
+        {!isLoading && moviesList?.length === 0 && !error && (
+          <p>No Movies Found</p>
+        )}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <div className="loader"></div>}
-        <MoviesList movies={moviesList} />
       </section>
     </React.Fragment>
   );
